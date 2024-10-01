a7743d277128ca7e79f86127dcd6bb97d7d8911a
dave2281 <dava.bigboy@gmail.com>
2024-10-01 09:05:58 +0300

    smth

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,8 +8,7 @@ class CommentsController < ApplicationController
     @category = Category.find(Question.find(params[:question_id]).category_id)
   end
 
-  def edit
-  end
+  def edit; end
 
   def create
     @comment = @question.comments.new(comment_params)
@@ -17,20 +16,24 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to category_question_path(@question.category_id, @question.id), notice: "Comment was successfully created." }
+        format.html do
+          redirect_to category_question_path(@question.category_id, @question.id),
+                      notice: 'Comment was successfully created.'
+        end
         format.json { render :show, status: :created, location: @comment }
       else
-        format.html { redirect_to category_question_path(@question.category_id, @question.id), alert: "Error creating comment." }
+        format.html do
+          redirect_to category_question_path(@question.category_id, @question.id), alert: 'Error creating comment.'
+        end
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
   end
 
-
   def update
     respond_to do |format|
       if @comment.update(comment_params)
-        format.html { redirect_to @comment, notice: "Comment was successfully updated." }
+        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
         format.json { render :show, status: :ok, location: @comment }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -46,12 +49,16 @@ class CommentsController < ApplicationController
     @comment.destroy!
 
     respond_to do |format|
-      format.html { redirect_to category_question_path(@category, @question), status: :see_other, notice: "Comment was successfully destroyed." }
+      format.html do
+        redirect_to category_question_path(@category, @question), status: :see_other,
+                                                                  notice: 'Comment was successfully destroyed.'
+      end
       format.json { head :no_content }
     end
   end
 
   private
+
   def set_question
     @question = Question.find(params[:question_id])
   end
