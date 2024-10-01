2799600928da4b756ce2c9a32112832700960bdb
dave2281 <dava.bigboy@gmail.com>
2024-10-01 18:06:16 +0300

    fixed footer
    footer color

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,33 +8,36 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-
 User.create!(
   [
-    { email: 'admin@example.com', password: 'password123', role: 'admin' },
-    { name: 'Андрей', surname: "Имейлович", email: 'user@example.com', password: 'password123', role: 'member' },
-    { name: "Билл", surname: "Гейтс", patronymic: "Генри", description: "University of Veterinary",
-      experience: '20 лет и более 10 тысяч вылеченных животных', role: 'doctor', email: 'john@gmail.com', password: 'password123'}
+    { name: 'Admin', surname: 'Adminskiy', email: 'admin@example.com', password: 'password123', role: 'admin' },
+    { name: 'Андрей', surname: 'Имейлович', email: 'user@example.com', password: 'password123', role: 'member' },
+    { name: 'Билл', surname: 'Гейтс', patronymic: 'Генри', description: 'University of Veterinary',
+      experience: '20 лет и более 10 тысяч вылеченных животных', role: 'doctor', email: 'john@gmail.com', password: 'password123' }
   ]
 )
 
 Category.create(
   [
-    { title: 'Коты', description: 'полезная информация о клинической симптоматике, опыте и методах лечения болезней у кошек', user_id: 1  },
-    { title: 'Собаки', description: 'полезная информация о клинической симптоматике, опыте и методах лечения болезней у собак', user_id: 1 }
+    { title: 'Коты',
+      description: 'полезная информация о клинической симптоматике, опыте и методах лечения болезней у кошек', user_id: 1 },
+    { title: 'Собаки',
+      description: 'полезная информация о клинической симптоматике, опыте и методах лечения болезней у собак', user_id: 1 }
   ]
 )
 
 Question.create!(
   [
-    { title: "На собаке появился 'лишай'", text: "Подскажите пожалуйста наиболее эффективный способ лечения.", user_id: 2, category_id: 2},
-    { title: "Кот очень вредный, какает мимо лотка.", text: "Подскажите пожалуйста наиболее эффективный способ лечения.", user_id: 2, category_id: 1}
- ]
+    { title: "На собаке появился 'лишай'", text: 'Подскажите пожалуйста наиболее эффективный способ лечения.',
+      user_id: 2, category_id: 2 },
+    { title: 'Кот очень вредный, какает мимо лотка.',
+      text: 'Подскажите пожалуйста наиболее эффективный способ лечения.', user_id: 2, category_id: 1 }
+  ]
 )
 
 Comment.create!(
   [
-    { text: "Да, ваш кот определенно вредный. Наиболее эффективный метод лечения, кусь за ховост. Ежедневно, а желательно 2-3 раза в день.",
+    { text: 'Да, ваш кот определенно вредный. Наиболее эффективный метод лечения, кусь за ховост. Ежедневно, а желательно 2-3 раза в день.',
       user_id: 3, question_id: 2 },
     { text: "Используйте 'Лишамид', намазывая на место укуса.", user_id: 3, question_id: 1 }
   ]
