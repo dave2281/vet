e73fb74bdc2f9adf0627e033f37c6fca7e84de2a
dave2281 <dava.bigboy@gmail.com>
2024-10-01 10:56:14 +0300

    just something

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,12 +1,12 @@
 class CategoriesController < ApplicationController
-  before_action :set_category, only: %i[ show edit update destroy ]
+  before_action :set_category, only: %i[show edit update destroy]
 
   def index
     @categories = Category.order(created_at: :asc)
   end
 
   def show
-    @pagy, @questions = pagy(Question.where(category_id: params[:id]).order(created_at: :desc), limit:3)
+    @pagy, @questions = pagy(Question.where(category_id: params[:id]).order(created_at: :desc), limit: 3)
     @users = User.all
   end
 
@@ -14,15 +14,14 @@ class CategoriesController < ApplicationController
     @category = Category.new
   end
 
-  def edit
-  end
+  def edit; end
 
   def create
     @category = Category.new(category_params)
 
     respond_to do |format|
       if @category.save
-        format.html { redirect_to @category, notice: "Category was successfully created." }
+        format.html { redirect_to @category, notice: 'Category was successfully created.' }
         format.json { render :show, status: :created, location: @category }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -34,7 +33,7 @@ class CategoriesController < ApplicationController
   def update
     respond_to do |format|
       if @category.update(category_params)
-        format.html { redirect_to @category, notice: "Category was successfully updated." }
+        format.html { redirect_to @category, notice: 'Category was successfully updated.' }
         format.json { render :show, status: :ok, location: @category }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -47,17 +46,20 @@ class CategoriesController < ApplicationController
     @category.destroy!
 
     respond_to do |format|
-      format.html { redirect_to unauthenticated_root_path, status: :see_other, notice: "Category was successfully destroyed." }
+      format.html do
+        redirect_to unauthenticated_root_path, status: :see_other, notice: 'Category was successfully destroyed.'
+      end
       format.json { head :no_content }
     end
   end
 
   private
-    def set_category
-      @category = Category.find(params[:id])
-    end
 
-    def category_params
-      params.require(:category).permit(:title, :description, :user_id)
-    end
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:title, :description, :user_id)
+  end
 end
