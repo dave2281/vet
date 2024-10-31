2a11da94b987396efc5a554ef94312969b783cc5
dave2281 <dava.bigboy@gmail.com>
2024-10-31 18:24:09 +0200

    routes admin namespace

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
       resources :comments
     end
   end
+
+  namespace :admin do
+    resources :dashboard, only: [:index]
+  end
+
   resources :users
 
   devise_scope :user do
@@ -16,11 +21,6 @@ Rails.application.routes.draw do
       root to: 'devise/sessions#new', as: :unauthenticated_root
     end
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  
   get "up" => "rails/health#show", as: :rails_health_check
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
